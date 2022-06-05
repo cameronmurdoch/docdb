b0811f25c4bee27a3e5010320c6c699a58c7d4cd
Cameron Murdoch <cam@macaroon.net>
2022-06-05 11:34:29 +0200

    Add some mock data to begin testing things

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type collection struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var collections = []collection{
+	{ID: "2f1f71e5-e149-4cc7-b23a-3c3c3d39a4ee", Name: "default"},
+	{ID: "6743c744-1a06-462e-82e6-85c9d0b2399f", Name: "Test"},
+	{ID: "bc1f09ae-f9cc-45cd-b265-8b6950c5b714", Name: "Weatherdata"},
+}
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -16,10 +28,11 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func Collections(w http.ResponseWriter, r *http.Request) {
+	response, _ := json.Marshal(collections)
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-
-	io.WriteString(w, `{"hello": "world"}`)
+	w.Write(response)
 }
 
 func main() {
@@ -29,3 +42,7 @@ func main() {
 	api.HandleFunc("/collections", Collections)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+	// e6bbb1d6-2cfc-4271-b8f9-ed35b600f368
+	// 02b1890f-7668-4d1c-aa09-21586906131b
+	// 0fb890af-f2bf-44a7-8662-5771049e3cb6
